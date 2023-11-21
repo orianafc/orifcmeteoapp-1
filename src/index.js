1ba5da8c01aad675905cbed1f6eb4f3dcc87a378
orianafc <ori_f93@hotmail.com>
2023-11-21 10:46:46 +0100

    Added JS function to capitalize the first letter of the input city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function capitalizeEachWord(string) {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city-input");
   let weatherCity = document.querySelector("#weather-city");
   weatherCity.innerHTML = searchInput.value;
+  if (searchInput.value) {
+    let inputCityCapitalize = capitalizeEachWord(searchInput.value);
+    weatherCity.innerHTML = inputCityCapitalize;
+  }
 }
 
 let searchFormElement = document.querySelector("#search-form");
