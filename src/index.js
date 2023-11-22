03cf99dad7637d25f3071d05373c8fbe1b90456b
orianafc <ori_f93@hotmail.com>
2023-11-22 11:25:37 +0100

    Added JS humidity, wind speed and feels function

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ function refreshWeather(response) {
   cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let feelsLike = response.data.temperature.feels_like;
+  feelsLikeElement.innerHTML = `${Math.round(feelsLike)}ºC`;
 }
+
 function searchCity(city) {
   let apiKey = "02ab91betd0a6efa2b010d3034deffo4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city-input");
